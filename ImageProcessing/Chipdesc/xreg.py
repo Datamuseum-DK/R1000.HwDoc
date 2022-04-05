f52e92685532517c74acd6b43dbba22736822818
Poul-Henning Kamp <phk@FreeBSD.org>
2022-04-05 05:49:16 +0000

    Fix XREG and XLAT pin names

--- ImageProcessing/Chipdesc/xreg.py
+++ ImageProcessing/Chipdesc/xreg.py
@@ -24,7 +24,7 @@ class XREG(Chip):
                 self.symbol += '  %|    xnn    |%\n'
             else:
                 self.symbol += '  %|           |%\n'
-            self.symbol += '-->+%-4s   %4s+===\n' % ("I%d" % (i-1), "Y%d" % (i-1))
+            self.symbol += '-->+%-4s   %4s+===\n' % ("D%d" % (i-1), "Q%d" % (i-1))
         self.symbol += '   |    _      |\n'
         self.symbol += '   +-----------+\n'
         super().__init__()
@@ -32,7 +32,7 @@ class XREG(Chip):
     def other_macros(self, file):
         file.write("#define %s_PINLIST" % self.symbol_name)
         for pin in self.pins:
-            if "Y" in pin.name:
+            if "Q" in pin.name:
                 file.write(" \\\n\tsc_out <sc_logic> pin%s;" % pin.number)
             else:
                 file.write(" \\\n\tsc_in <sc_logic> pin%s;" % pin.number)
@@ -41,7 +41,7 @@ class XREG(Chip):
         file.write("#ifdef ANON_PINS\n")
         file.write("#define PIN_PAIRS(macro)")
         for i in range(1, self.xreg_npins + 1):
-            file.write(" \\\n\tmacro(%d, PIN_I%d, PIN_Y%d)" % (self.xreg_npins - i, (i-1), (i-1)))
+            file.write(" \\\n\tmacro(%d, PIN_D%d, PIN_Q%d)" % (self.xreg_npins - i, (i-1), (i-1)))
         file.write("\n")
         file.write("#endif\n")
            
