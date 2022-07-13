e71aae2813a8d792c20f70ff85f7b0b0e0d70562
Poul-Henning Kamp <phk@FreeBSD.org>
2022-07-13 07:26:51 +0000

    Megacom WCS + parity

--- ImageProcessing/Chipdesc/xwcsram.py
+++ ImageProcessing/Chipdesc/xwcsram.py
@@ -8,58 +8,52 @@ class XWCSRAM(Chip):
 
     ''' WCS RAM 16384x8 '''
 
-    symbol_name = "XWCSRAM"
+    def __init__(self, width):
 
-    symbol = '''
+        self.symbol_name = "XWCSRAM"
+        if width != 8:
+            self.symbol_name += "%d" % width
+
+        self.symbol = '''
         |
         |
-      31v
+       %v
    +----o---+
    |   WE   |
-  1|        |2
--->+D0    Q0+-->
-  3|        |4
--->+D1    Q1+-->
-  5|        |6
--->+D2    Q2+-->
-  7|        |8
--->+D3    Q3+-->
-  9|        |10
--->+D4    Q4+-->
- 11|        |12
--->+D5    Q5+-->
- 13|        |14
--->+D6    Q6+-->
- 15|        |16
--->+D7    Q7+-->
-   |        |
- 17|        |
+'''
+
+        for i in range(width):
+            self.symbol += "  %|        |%\n"
+            self.symbol += "-->+D%-2d  %3s+-->\n" % (i, "Q%d" % i)
+
+        self.symbol += '''   |        |
+  %|        |
 -->+A0      |
- 18|        |
+  %|        |
 -->+A1      |
- 19|        |
+  %|        |
 -->+A2      |
- 20|        |
+  %|        |
 -->+A3      |
- 21|        |
+  %|        |
 -->+A4      |
- 22|        |
+  %|        |
 -->+A5      |
- 23|        |
+  %|        |
 -->+A6      |
- 24|        |
+  %|        |
 -->+A7      |
- 25|        |
+  %|        |
 -->+A8      |
- 26|        |
+  %|        |
 -->+A9      |
- 27|        |
+  %|        |
 -->+A10     |
- 28|        |
+  %|        |
 -->+A11     |
- 29|        |
+  %|        |
 -->+A12     |
- 30|        |
+  %|        |
 -->+A13     |
    |        |
    |  xnn   |
@@ -67,5 +61,12 @@ class XWCSRAM(Chip):
    +--------+
 '''
 
+        super().__init__()
+
 if __name__ == "__main__":
-    XWCSRAM(__file__).main()
+    XWCSRAM(8).main()
+    # XWCSRAM(16).main()
+    XWCSRAM(39).main()
+    XWCSRAM(40).main()
+    XWCSRAM(42).main()
+    XWCSRAM(47).main()
