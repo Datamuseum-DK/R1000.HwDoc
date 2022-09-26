fb7c53a94e506737649a90f784b1a019eef883ae
Poul-Henning Kamp <phk@FreeBSD.org>
2022-09-26 19:48:56 +0000

    XHASH

--- ImageProcessing/Chipdesc/xhash.py
+++ ImageProcessing/Chipdesc/xhash.py
@@ -14,28 +14,28 @@ class XHASH(Chip):
         l = []
         l.append('   +-----------+')
 
-        hash = [0]
+        hash = [-3]
         def sect(let, low, pins):
             # l.append('   |           |')
             for pin in range(low, low + pins):
                 pn = '-->+%-3s' % (let + "%d" % pin)
-                if hash[0] < 12:
+                if 0 <= hash[0] < 12:
                     l.append('  %|           |%')
-                    l.append(pn + '  %6s+-->' % ("HASH%d" % hash[0]))
-                    hash[0] += 1
+                    l.append(pn + '  %6s+-->' % ("H%d" % hash[0]))
+                elif 24 <= pin <= 27:
+                    l.append('  %|           |%')
+                    l.append(pn + '      B%d+-->' % (pin - 24))
                 else:
                     l.append('  %|           |')
                     l.append(pn + '        |')
+                hash[0] += 1
+            l.append('   |           |')
+            l.append('   |           |')
+            l.append('   |           |')
+            l.append('   |           |')
 
         sect("S", 0, 3)
-        sect("N", 0, 8)
-        sect("N", 8, 8)
-        sect("N", 16, 8)
-        sect("N", 24, 8)
-        sect("O", 0, 8)
-        sect("O", 8, 11)
-        sect("O", 19, 6)
-        l.append('   |           |')
+        sect("A", 0, 32 + 24)
         l.append('   |  xnn      |')
         l.append('   |           |')
         l.append('   | _         |')
