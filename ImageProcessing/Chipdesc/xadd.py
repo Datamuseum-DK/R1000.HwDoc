f699372d8a05e8503e7b854a844c0ab3013453a9
Poul-Henning Kamp <phk@FreeBSD.org>
2022-09-30 04:29:47 +0000

    Tested to 9.16 seconds (rc017)

--- ImageProcessing/Chipdesc/xadd.py
+++ ImageProcessing/Chipdesc/xadd.py
@@ -31,44 +31,6 @@ class XADD(Chip):
         self.symbol += '   +--------+\n'
         super().__init__()
 
-    def other_macros(self, file):
-        file.write("#define %s_PINLIST" % self.symbol_name)
-        for pin in self.pins:
-            if "Y" in pin.name:
-                file.write(" \\\n\tsc_out <sc_logic> pin%s;" % pin.number)
-            elif "CO" in pin.name:
-                file.write(" \\\n\tsc_out <sc_logic> pin%s;" % pin.number)
-            else:
-                file.write(" \\\n\tsc_in <sc_logic> pin%s;" % pin.number)
-        file.write("\n")
-        file.write("\n")
-        file.write("#ifdef ANON_PINS\n")
-
-        file.write("\n")
-        file.write("#define PIN_SETS_A(macro)")
-        for i in range(0, self.xreg_npins):
-            file.write(" \\\n\tmacro(%d, PIN_A%d)" % (self.xreg_npins - (i+1), i))
-        file.write("\n")
-
-        file.write("\n")
-        file.write("#define PIN_SETS_B(macro)")
-        for i in range(0, self.xreg_npins):
-            file.write(" \\\n\tmacro(%d, PIN_B%d)" % (self.xreg_npins - (i+1), i))
-
-        file.write("\n")
-        file.write("#define PIN_SETS_IN(macro)")
-        file.write(" \\\n\tPIN_SETS_A(macro)")
-        file.write(" \\\n\tPIN_SETS_B(macro)")
-        file.write(" \\\n\tmacro(0, PIN_CI)\n")
-        file.write("\n")
-
-        file.write("\n")
-        file.write("#define PIN_SETS_OUT(macro)")
-        file.write(" \\\n\tmacro(%d, PIN_CO)" % self.xreg_npins)
-        for i in range(0, self.xreg_npins):
-            file.write(" \\\n\tmacro(%d, PIN_Y%d)" % (self.xreg_npins - (i+1), i))
-        file.write("\n")
-        file.write("#endif\n")
-           
 if __name__ == "__main__":
     XADD(8).main()
+    XADD(14).main()
